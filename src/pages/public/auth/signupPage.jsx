c11fa500153381b779b71197af474540539f2a4c
djoKadima <djokadimatshibangu@gmail.com>
2024-05-05 12:56:10 +0100

    creat signup page

--- src/pages/public/auth/signupPage.jsx
+++ src/pages/public/auth/signupPage.jsx
@@ -11,26 +11,26 @@ const SignupPage = () =>{
       console.log(data);
     return(
         <div>
-        <h2>Page d&apos;Inscription</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label>Nom:</label>
-            <input {...register("name", { required: true })} />
-            {errors.name && <span>Ce champ est requis.</span>}
-          </div>
-          <div>
-            <label>Email:</label>
-            <input type="email" {...register("email", { required: true })} />
-            {errors.email && <span>Une adresse email valide est requise.</span>}
-          </div>
-          <div>
-            <label>Mot de passe:</label>
-            <input type="password" {...register("password", { required: true })} />
-            {errors.password && <span>Ce champ est requis.</span>}
-          </div>
-          <button type="submit">S&apos;Inscrire</button>
-        </form>
-      </div>
+                <h2>Page d&apos;Inscription</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label>Nom:</label>
+                        <input {...register("name", { required: true })} />
+                        {errors.name && <span>Ce champ est requis.</span>}
+                    </div>
+                    <div>
+                        <label>Email:</label>
+                        <input type="email" {...register("email", { required: true })} />
+                        {errors.email && <span>Une adresse email valide est requise.</span>}
+                    </div>
+                    <div>
+                        <label>Mot de passe:</label>
+                        <input type="password" {...register("password", { required: true })} />
+                        {errors.password && <span>Ce champ est requis.</span>}
+                    </div>
+                    <button type="submit">S&apos;Inscrire</button>
+              </form> 
+         </div>
     );
   }
   
